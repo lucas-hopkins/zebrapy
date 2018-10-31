eb4a6ab5ca44c6a1cabd97f1695062d466fd18aa
Lucas Hopkins <lucas.hopkins@42-q.com>
2018-10-31 16:22:58 -0500

    Initial commit

---
+++ Zbt.py
@@ -0,0 +1,40 @@
+import bluetooth
+
+
+class Client:
+
+    def __init__(self, sock=None,):
+        self.sock = None
+
+        if sock is None:
+            self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        else:
+            self.sock = sock
+
+    def connect(self,host="AC:3F:A4:80:67:FD",port=1):
+        self.host = host
+        self.port = port
+
+        try:
+            print("Attempting Connection...")
+            # Create client socket
+            self.sock.connect((host, port))
+        except bluetooth.btcommon.BluetoothError as error:
+            print("Could not connect: ", error, "; Retrying in 5s...")
+
+
+    def mysend(self):
+
+        self.sock.send('! U1 getvar "comm.baud"\n')
+
+
+    def myreceive(self):
+        data = self.sock.recv(32768)
+        print(data)
+
+
+
+c= Client()
+c.connect()
+
+
