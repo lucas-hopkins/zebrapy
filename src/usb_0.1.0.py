53411a5588f0a08eae1f594c47ee9f51292d7f37
lucas <lucas.hopkins@42-q.com>
2019-02-07 09:30:08 -0600

    Added startup screen

---
+++ src/usb_0.1.0.py
@@ -0,0 +1,129 @@
+#!/home/lucas/.virtualenvs/usb_dev/bin/python3
+
+from datetime import datetime
+import binascii
+import sys
+#import codecs
+#from colorama import init
+#init(strip=not sys.stdout.isatty())
+#from termcolor import cprint
+from pyfiglet import Figlet
+import time
+import usb.core
+import usb.util
+
+
+dev = usb.core.find(idVendor=0xa5f)
+#print(dev)
+        
+def connect_to_printer():
+#was the device found
+    if dev is None:
+        raise ValueError('Device not found')
+    if dev.is_kernel_driver_active(0):
+        print("Detaching kernel driver")
+        reattach = True
+        dev.detach_kernel_driver(0)
+
+
+#### show config / interface / endpoints: #################################################
+    #for config in dev:
+    #    sys.stdout.write('Config ' + str(config.bConfigurationValue) + '\n')
+    #for interface in config:
+    #    sys.stdout.write('\t' + \
+    #        'Interface ' + str(interface.bInterfaceNumber) + \
+    #                     ',' + \
+    #                     str(interface.bAlternateSetting) + \
+    #                     '\n')
+    #    for endpoint in interface:
+    #        sys.stdout.write('\t\t' + \
+    #                        'Endpoint ' + str(endpoint.bEndpointAddress) + \
+    #                         '\n')
+############################################################################################    
+
+def get_endpoints(dev):
+    global epo 
+    global epi
+
+    dev.set_configuration()
+
+# get an endpoint instance
+    cfg = dev.get_active_configuration()
+    intf = cfg[(0, 0)]
+
+    epo = usb.util.find_descriptor(
+        intf,
+# match the first OUT endpoint
+        custom_match= \
+            lambda e: \
+                usb.util.endpoint_direction(e.bEndpointAddress) == \
+                usb.util.ENDPOINT_OUT)
+    #print(epo)
+    
+    #match the first IN endpoint
+    epi = usb.util.find_descriptor(
+        intf,
+        custom_match= \
+            lambda e: \
+                usb.util.endpoint_direction(e.bEndpointAddress) == \
+                usb.util.ENDPOINT_IN)
+    #print(epi)
+
+
+# Write to the Printer
+def send_command():
+
+    cmd_string = input(" Enter a command or press \'q\' to quit" + '\r\n')
+    if cmd_string is not '' and not 'q': 
+        cmd_string = (cmd_string + '\r\n')        
+        print("\ncmd_string:", cmd_string)
+        cmd_bytes = bytearray(cmd_string.encode('utf-8'))
+        for cmd_byte in cmd_bytes:
+            hex_byte = ("{0:02x}".format(cmd_byte)) 
+            #hex_bytes = cmd_bytes.hex()
+            dev.write(1, bytearray.fromhex(hex_byte))
+            time.sleep(1.0)
+
+           #print time for debugging
+            t = datetime.utcnow()
+            print(t)
+    else: 
+        sys.exit()
+
+        #read the response
+
+def read_response():
+    try:
+        for x in range (0, 1): #remove as it serves no purpose
+            while 1:
+                ret = dev.read(epi, 1024, 5000)
+                print("response: ", binascii.hexlify(bytearray(ret)))
+                ret =str(ret, 'utf-8')
+                print(ret)
+                usb.util.dispose_resources(dev)
+    except usb.core.USBError as e:
+        print(e)
+        # This is needed to release interface, otherwise attach_kernel_driver fails
+        # due to "Resource busy"
+
+
+def main(): 
+    f = Figlet(font='standard', width=1920)
+    print(f.renderText('42Q Configurator'))
+    for x in range (0, 5):
+        print('\n')
+    #removed cmd_name from the arguments
+    #cmd_name ='get_ip'
+    #cmd_string ='! U1 getvar "ip.addr" ""\r\n'
+    connect_to_printer()
+    get_endpoints(dev)
+    while True:
+        send_command()
+        read_response()
+        dev.reset()
+
+if __name__ == '__main__':
+    main()
+# It may raise USBError if there's e.g. no kernel driver loaded at all
+#if reattach:
+#    dev.attach_kernel_driver(0)
