6dc4135232f5a6615fe9774a00d5b4cc5f435eb5
lucas <lucas.hopkins@42-q.com>
2019-02-15 22:14:10 -0600

    Push

--- src/usb_0.1.0.py
+++ src/usb_0.1.0.py
@@ -1,56 +1,46 @@
 #!/home/lucas/.virtualenvs/usb_dev/bin/python3
 
-# Test Commands
-
-#! U1 getvar "ip.addr" ""
-
+"""This script allows you to communicate with a Zebra printer over USB or TCP/IP"""
 
 from datetime import datetime
 # import argparse -------------Eventually implement for accessing
 # functionality directly
 import binascii
+import threading
+import queue
 import sys
 # import codecs
 # from colorama import init  - For Figlet color support
 # init(strip=not sys.stdout.isatty())
 # from termcolor import cprint - For Windows color support
-from pyfiglet import Figlet
 import time
 import subprocess
+from pyfiglet import Figlet
 import usb.core
 import usb.util
 
 
 class zebraPrinter:
 
+    def __init__(self, dev):  # define the constructor
 
-    def __init__(self):  # define the constructor
-
-        self.dev = None  
-        self.cmd_strings = []
-        cmd_strings = self.cmd_strings
-        # self.set_configuration()
-        # self.get_out_endpoints()
-        # self.get_in_endpoints()
-        # self.get_commands()
-        # self.send_to_printer()
-        # self.read_response()
+        self.dev = dev
+        self.cmd_strings = ''
+        #cmd_strings = self.cmd_strings
 
     def get_printer(self):
-        
+
         try:
-            dev = self.dev
-            # find all printers with the Zebra id (This supposes there is only one)
-            dev = usb.core.find(idVendor=0xa5f)
-            # was the device found
-            if self.dev is None:
-                except ValueError as e 
-                print('Device not found')
-                print("Please plug in the printer")
+         # find all printers with the Zebra id (This supposes there is only
+         # one)
+            self.dev = usb.core.find(idVendor=0xa5f)
+            # if self.dev is None:
             if self.dev.is_kernel_driver_active(0):
                 print("Detaching kernel driver")
                 reattach = True
                 self.dev.detach_kernel_driver(0)
+        except ValueError:
+            print('Device not found')
 
 
 #### show config / interface / endpoints: ################################
@@ -71,80 +61,55 @@ class zebraPrinter:
     def set_configuration(self):
 
         self.dev.set_configuration()
-        # get an endpoint instance
         cfg = self.dev.get_active_configuration()
         self.intf = cfg[(0, 0)]
-
         return self.intf
 
-    def get_in_endpoints(self):
+    def get_out_endpoints(self):
 
         epo = usb.util.find_descriptor(
             self.intf,
-            # match the first OUT endpoint
-            custom_match=lambda e: \
-            usb.util.endpoint_direction(e.bEndpointAddress) == \
+            custom_match=lambda e:
+            usb.util.endpoint_direction(e.bEndpointAddress) ==
             usb.util.ENDPOINT_OUT)
-        # print(epo)
         return epo
 
-    def get_out_endpoints(self):
-        # match the first IN endpoint
+    def get_in_endpoints(self):
         epi = usb.util.find_descriptor(
             self.intf,
             custom_match=lambda e:
             usb.util.endpoint_direction(e.bEndpointAddress) ==
             usb.util.ENDPOINT_IN)
-        # print(epi)
         return epi
 
     # Write to the Printer
-    def get_commands(self): # --------------------Work here ------------ Not finished. Need to update for multiline input
-
-        print(" Enter command(s) or press \'q\' to quit" + '\r\n')
-        print("Once all the desired commands are entered, press Ctrl + D to submit")
-        
-        while True:
-            try:
-                self.cmd_strings = []
-                cmd_string = input()
-                if cmd_string == 'q':
-                    sys.exit()
-                elif cmd_string == '\r':
-                    continue
-                elif cmd_string == '':
-                    continue
-                else:
-                    self.cmd_strings = self.cmd_strings.append(cmd_string)
-                    continue
-            except EOFError:
-                yield self.cmd_strings
-
-    # def get_file(self):               --------------------- Implement later
+    # def get_file(self):  --------------------- Implement later
     # def get_commands_from_file(self): --------------------- Implement later
 
-    def send_to_printer(self, cmd_strings):
-        cmd_strings = self.get_commands()
-        for cmd_string in cmd_strings:
-            cmd_string = (cmd_string + '\r\n')
-            print("\ncmd_string:", cmd_string)
-            cmd_bytes = bytearray(cmd_string.encode('utf-8'))
-            for cmd_byte in cmd_bytes:
-                hex_byte = ("{0:02x}".format(cmd_byte))
+    def send_to_printer(self, cmd):
+        print("Sending....")
+        print(cmd)
+        cmds = (cmd + '\r\n')
+        print("\ncmd_string:", cmds)
+        cmd_bytes = bytearray(cmds.encode('utf-8'))
+        for cmd_byte in cmd_bytes:
+            hex_byte = ("{0:02x}".format(cmd_byte))
             # hex_bytes = cmd_bytes.hex()
-                self.dev.write(1, bytearray.fromhex(hex_byte))
-                time.sleep(1.0)
+            self.dev.write(
+                self.get_out_endpoints(),
+                bytearray.fromhex(hex_byte))
+            time.sleep(2.0)
 
             # print time for debugging
-            t = datetime.utcnow()
-            print(t)
+        t = datetime.utcnow()
+        print(t)
 
  # read the response
 
     def read_response(self):
         try:
             for x in range(0, 1):  # remove as it serves no purpose
-                while 1:
+                while True:
                     ret = self.dev.read(self.get_in_endpoints(), 1024, 5000)
                     print("response: ", binascii.hexlify(bytearray(ret)))
                     ret = str(ret, 'utf-8')
@@ -152,14 +117,24 @@ class zebraPrinter:
                     usb.util.dispose_resources(self.dev)
         except usb.core.USBError as e:
             print(e)
-        # This is needed to release interface, otherwise attach_kernel_driver fails
-        # due to "Resource busy"
+
+#    def worker(self, in_q, out_q):
+#        abort = False
+#        while not abort:
+#            try:
+#                task = in_q.get(True, .5)
+#            except Queue.Empty:
+#                abort = True
+#            else:
+#                response = task
+#                out_q.put(response)
+#                in_q.task_done()
 
 
 class menuHandler():
 
     def menu(self):
-        f = Figlet(font='standard', width=1920)
+        f = Figlet(font='standard', width=440)
         print(f.renderText('42Q Configurator'))
 
         for x in range(0, 3):
@@ -176,56 +151,62 @@ class menuHandler():
         return i
 
     def help_page(self):
-        
+
         subprocess.call(["vi", "-R", "help.txt"])
-        f = Figlet(font='standard', width=1920)
-        print(f.renderText('Help'))
 
-        for x in range(0, 3):
-            print('\n')
-
-        f = open('help.txt')
-        print(f.read())
-        f.close()
-
-        
         while True:
             print("Press 'r' to return to the menu or 'q' to quit")
             i = input()
             if i == 'r':
                 main()
-            #elif i == '\r':
-            #    i = input()
             elif i == 'q':
                 sys.exit()
             else:
                 print("I'm sorry that's not a valid option")
-            # i = input()
 
 
 def main():
     m = menuHandler()
-    z = zebraPrinter()
+    dev = None
+    z = zebraPrinter(dev)
 
-    while True:
-        m.menu()
-        choice = m.getChoice()
-        if choice == 'c':
-            m.help_page()
-        if choice == 'u':
-            z.get_printer()
-            z.set_configuration()
-            z.get_out_endpoints()
-            z.get_in_endpoints()
-            # z.get_commands()
-            z.send_to_printer(z.get_commands)
-            z.read_response()
-        else:
-            print('Feature not implemented - Check back soon')
+    m.menu()
+    choice = m.getChoice()
+    if choice == 'c':
+        m.help_page()
+    if choice == 'u':
+        z.get_printer()
+        z.set_configuration()
+        cout = z.get_out_endpoints()
+        print(cout)
+        cin = z.get_in_endpoints()
+        print(cin)
+        print(" Enter command(s) or press \'q\' to quit" + '\r\n')
+        print(
+            "Once all the desired commands are entered, press Ctrl + D to submit" +
+            '\r\n')
+        cmds = []
+        while True:
+            try:
+                cmd = input()
+                cmds.append(cmd)
+                c = cmds
+                print(cmds)
+            except EOFError:
+                break
+
+        for cmd in cmds:
+            t = threading.Thread(name='send_to_printer', target=z.send_to_printer(cmd))
+            t.start()
+            #z.send_to_printer(cmd)
+            t2 = threading.Thread(name='read', target=z.read_response())
+            t2.start()
+        # print(" Select an option") -------------------------Return loop to
+        # menu
+        usb.util.dispose_resources(dev)
+    else:
+        print('Feature not implemented - Check back soon')
 
 
 if __name__ == '__main__':
     main()
-# It may raise USBError if there's e.g. no kernel driver loaded at all
-# if reattach:
-# dev.attach_kernel_driver(0)
